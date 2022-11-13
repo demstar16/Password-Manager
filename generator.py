3546e7cbcdeab7977674100d49d185d9371f3f8c
demstar16 <44187160+demstar16@users.noreply.github.com>
2022-11-13 21:56:16 +0800

    fixed funtinality

--- generator.py
+++ generator.py
@@ -24,19 +24,20 @@ password_len = 12
 
 password = "".join(random.sample(valid_characters, password_len))
 
-data = []
+data = name + "," + password + '\n'
+#data.strip('\n')
+
 
 # open the file to read, to make sure that there isn't already a password
 try:
     with open('passwords.csv', 'r') as fp:
-        csv_reader = reader(fp, delimiter=',')
-        for row in csv_reader:
-            if row[0] == name:
+        #csv_reader = reader(fp, delimiter=',')
+        for row in fp:
+            current_row = row.strip('\n').split(",")
+            if current_row == [] or current_row == '': continue
+            if current_row[0] == name:
                 print(f'password already exists for that item')
-                exit(0)
-
-        data.append(name)
-        data.append(password)    
+                exit(0)    
         fp.close()
 except FileNotFoundError:
     print("you don't have a passwords file... we will create one for you...\npasswords.csv has been generated.\n please run the program and try again")
@@ -45,6 +46,7 @@ except FileNotFoundError:
 
 # Write the contents to the file
 fp = open('passwords.csv', 'a')
-writer_object = writer(fp)
-writer_object.writerow(data)
+fp.write(data)
 fp.close()
+
+print(f"Password for {name} has successfully been generated")
